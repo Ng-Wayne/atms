84af26e52bedaa74d070e700fd1767cd61aed3b8
Wayne Ng <ngzuwayne@gmail.com>
2026-05-11 17:40:40 +0800

    Wrote a failure test case with two withdrawals for the flow integration test class where the first withdrawal failed due to the minimum balance being violated and the subsequent withdrawal succeeded

--- src/test/java/com/wayneng/atms/integration/FlowTest.java
+++ src/test/java/com/wayneng/atms/integration/FlowTest.java
@@ -62,7 +62,7 @@ class FlowTest {
         account.setCurrency("MYR");
         account.setAvailableBalance(new BigDecimal("1000"));
         account.setLedgerBalance(new BigDecimal("1000"));
-        account.setMinimumBalance(BigDecimal.ZERO);
+        account.setMinimumBalance(new BigDecimal("100"));
         account.setDailyWithdrawalLimit(new BigDecimal("5000"));
         account.setCustomer(customer);
         account.setBank(bank);
@@ -217,6 +217,7 @@ class FlowTest {
                 .hasMessageContaining("Insufficient balance");
 
         Transaction tx = transactionService.getTransactionBySessionId(session.getSessionId());
+        assertThat(tx.getTransactionType()).isEqualTo("WITHDRAWAL");
         assertThat(tx.getTransactionStatus()).isEqualTo("FAILED");
 
         Account updatedAccount = accountRepository.findById(ACCOUNT_NUMBER).orElseThrow();
@@ -228,6 +229,51 @@ class FlowTest {
                 .isEqualByComparingTo("10000");
 
         sessionService.endSession(session.getSessionId(), "FAILED");
+        Session ended = sessionRepository.findById(session.getSessionId()).orElseThrow();
+        assertThat(ended.getSessionStatus()).isEqualTo("ENDED");
+    }
+
+    // FAILURE CASE - MINIMUM BALANCE VIOLATED (TWO WITHDRAWALS)
+    @Test
+    void shouldFailWithdrawal_dueToMinimumBalance() {
+
+        Session session = sessionService.startSession(CARD_NUMBER, ATM_CODE);
+        sessionService.authenticateSession(session.getSessionId());
+        BigDecimal withdrawAmount = new BigDecimal("900.01");
+
+        assertThatThrownBy(() ->
+                withdrawalService.withdraw(session, withdrawAmount)
+        ).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("Minimum balance violated");
+
+        Transaction tx = transactionService.getTransactionBySessionId(session.getSessionId());
+        assertThat(tx.getTransactionType()).isEqualTo("WITHDRAWAL");
+        assertThat(tx.getTransactionStatus()).isEqualTo("FAILED");
+
+        Account updatedAccount = accountRepository.findById(ACCOUNT_NUMBER).orElseThrow();
+        assertThat(updatedAccount.getAvailableBalance())
+                .isEqualByComparingTo("1000");
+
+        ATM updatedATM = atmRepository.findById(ATM_CODE).orElseThrow();
+        assertThat(updatedATM.getCashAvailable())
+                .isEqualByComparingTo("10000");
+
+        BigDecimal withdrawAmount2 = new BigDecimal("900");
+        withdrawalService.withdraw(session, withdrawAmount2);
+
+        Transaction tx2 = transactionService.getTransactionBySessionId(session.getSessionId());
+        assertThat(tx2.getTransactionType()).isEqualTo("WITHDRAWAL");
+        assertThat(tx2.getTransactionStatus()).isEqualTo("SUCCESS");
+
+        Account updatedAccount2 = accountRepository.findById(ACCOUNT_NUMBER).orElseThrow();
+        assertThat(updatedAccount2.getAvailableBalance())
+                .isEqualByComparingTo("100");
+
+        ATM updatedATM2 = atmRepository.findById(ATM_CODE).orElseThrow();
+        assertThat(updatedATM2.getCashAvailable())
+                .isEqualByComparingTo("9100");
+
+        sessionService.endSession(session.getSessionId(), "COMPLETED");
         Session ended = sessionRepository.findById(session.getSessionId()).orElseThrow();
         assertThat(ended.getSessionStatus()).isEqualTo("ENDED");
     }
