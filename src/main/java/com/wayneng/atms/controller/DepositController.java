5386bebf204ce71ba52ec65164e1d6919568ea99
Wayne Ng <ngzuwayne@gmail.com>
2026-05-12 09:27:40 +0800

    Added the deposit controller class

---
+++ src/main/java/com/wayneng/atms/controller/DepositController.java
@@ -0,0 +1,27 @@
+package com.wayneng.atms.controller;
+
+import com.wayneng.atms.dto.AmountRequest;
+import com.wayneng.atms.service.DepositService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/deposits")
+@RequiredArgsConstructor
+public class DepositController {
+
+    private final DepositService depositService;
+
+    @PostMapping
+    public ResponseEntity<String> withdraw(
+            @RequestBody AmountRequest request
+    ) {
+        try {
+            depositService.deposit(request.getSession(), request.getAmount());
+            return ResponseEntity.ok("Deposit successful");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Deposit failed: " + e.getMessage());
+        }
+    }
+}
