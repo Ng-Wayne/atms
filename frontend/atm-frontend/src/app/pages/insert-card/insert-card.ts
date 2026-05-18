f60b620237a25ca2cd503cc213fea11a037cc1e7
Wayne Ng <ngzuwayne@gmail.com>
2026-05-18 13:07:54 +0800

    Made it so that the user can type the card number into 16 separate boxes ... just a visual thing

--- frontend/atm-frontend/src/app/pages/insert-card/insert-card.ts
+++ frontend/atm-frontend/src/app/pages/insert-card/insert-card.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-insert-card',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './insert-card.html',
   styleUrl: './insert-card.css'
 })
 export class InsertCardComponent {
 
   cardNumber: string = '';
+
+  boxes = Array(16);
+
+  @ViewChild('hiddenInput') hiddenInput!: ElementRef;
+
+  focusInput() {
+    this.hiddenInput.nativeElement.focus();
+  }
 
   onInputChange() {
     this.cardNumber = this.cardNumber.replace(/\D/g, '');
