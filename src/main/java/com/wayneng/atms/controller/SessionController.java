f40c62cc946a07eb74ffc324fcfd1190ec9d5633
Wayne Ng <ngzuwayne@gmail.com>
2026-05-19 14:08:16 +0800

    Changed endpoint names in the controllers

--- src/main/java/com/wayneng/atms/controller/SessionController.java
+++ src/main/java/com/wayneng/atms/controller/SessionController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/sessions")
+@RequestMapping("/api/session")
 @RequiredArgsConstructor
 public class SessionController {
 
--- src/main/java/com/wayneng/atms/controller/WithdrawalController.java
+++ src/main/java/com/wayneng/atms/controller/WithdrawalController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/withdrawal")
+@RequestMapping("/api/withdraw")
 @RequiredArgsConstructor
 public class WithdrawalController {
 
