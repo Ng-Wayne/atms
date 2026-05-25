ec55fe7aa8bb6232f748a8e8b3f9a1d7fa8f52a1
Wayne Ng <ngzuwayne@gmail.com>
2026-05-25 14:23:42 +0800

    Added angular's url to the session controller to prevent cors issues

--- src/main/java/com/wayneng/atms/controller/SessionController.java
+++ src/main/java/com/wayneng/atms/controller/SessionController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/session")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class SessionController {
 
     private final SessionService sessionService;
