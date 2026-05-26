c574641f0a8b17bf7de2db06e3666242fdaaeb31
Wayne Ng <ngzuwayne@gmail.com>
2026-05-26 16:01:51 +0800

    Got the ui up for enter pin

--- frontend/atm-frontend/src/app/pages/enter-pin/enter-pin.ts
+++ frontend/atm-frontend/src/app/pages/enter-pin/enter-pin.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-enter-pin',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './enter-pin.html'
 })
-export class EnterPinComponent {}
+export class EnterPinComponent {
+
+  isPinFocused = false;
+  pin: string = '';
+  boxes = Array(6);
+
+  @ViewChild('pinInput') pinInput!: ElementRef;
+
+  constructor(private http: HttpClient) {}
+
+  focusPinInput() {
+    this.pinInput.nativeElement.focus();
+  }
+
+  onPinChange() {
+    this.pin = this.sanitizeNumericInput(this.pin, 6);
+  }
+
+  sanitizeNumericInput(value: string, maxLength: number): string {
+
+    value = value.replace(/\D/g, '');
+
+    if (value.length > maxLength) {
+      value = value.slice(0, maxLength);
+    }
+
+    return value;
+  }
+
+  submit() {
+
+    console.log(this.pin);
+  }
+}
