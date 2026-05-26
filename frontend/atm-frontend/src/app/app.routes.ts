09d803148447d5fea479783045621e941b422c53
Wayne Ng <ngzuwayne@gmail.com>
2026-05-26 14:39:28 +0800

    Created a new page for entering pin and set up the route

--- frontend/atm-frontend/src/app/app.routes.ts
+++ frontend/atm-frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { InsertCardComponent } from './pages/insert-card/insert-card';
+import { EnterPinComponent } from './pages/enter-pin/enter-pin';
 
 export const routes: Routes = [
   {
     path: '',
     component: InsertCardComponent
+  },
+  {
+    path: 'enter-pin',
+    component: EnterPinComponent
   }
 ];
---
+++ frontend/atm-frontend/src/app/pages/enter-pin/enter-pin.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-pin',
+  imports: [],
+  templateUrl: './enter-pin.html'
+})
+export class EnterPinComponent {}
