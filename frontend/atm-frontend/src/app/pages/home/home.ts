05d5ccf87c21501ed3385b1fa614a90d06e19406
Wayne Ng <ngzuwayne@gmail.com>
2026-06-12 17:22:20 +0800

    Added the route to the balance inquiry page in the home page

--- frontend/atm-frontend/src/app/pages/home/home.ts
+++ frontend/atm-frontend/src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './home.html'
 })
 export class HomeComponent {
+
+  constructor(
+      private router: Router
+    ) {}
+
+  balanceInquiry() {
+    this.router.navigate(['/balance-inquiry']);
+  }
 
 }
